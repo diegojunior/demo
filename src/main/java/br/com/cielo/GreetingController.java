e0629101bed8244a8ac4533353615753408296a7
Diego Junior <diego.junior10@gmail.com>
2016-11-30 14:32:39 -0200

    Teste

---
+++ src/main/java/br/com/cielo/GreetingController.java
@@ -0,0 +1,30 @@
+package br.com.cielo;
+
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cielo.json.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static final String TEMPLATE = "Hello, %s!";
+
+	@RequestMapping("/greeting")
+	public HttpEntity<Greeting> greeting(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+
+		Greeting greeting = new Greeting(String.format(TEMPLATE, name));
+		greeting.add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(GreetingController.class).greeting(name))
+				.withSelfRel());
+		
+
+		return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
+	}
+
+}
